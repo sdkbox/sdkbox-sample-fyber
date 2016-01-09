7905de538a503a656a12cfb5b3282e96c0610ad8
tinybearc <tinybearc@gmail.com>
2016-01-09 16:39:17 +0800

    update cpp

--- cpp/Classes/HelloWorldScene.cpp
+++ cpp/Classes/HelloWorldScene.cpp
@@ -2,18 +2,34 @@
 #include "HelloWorldScene.h"
 
 USING_NS_CC;
+using namespace sdkbox;
+
+#define DEFAULT_PLACEMENT_ID "coins"
+#define DEFAULT_CURRENCY_NAME "coins"
+
+static const char *BOOL_TO_STRING(bool ok)
+{
+    return ok ? "yes" : "no";
+}
+
+static std::string make_string_with_coins(double coins)
+{
+    char ret[64] = {0};
+    snprintf(ret, sizeof(ret), "%.1lf %s", coins, DEFAULT_CURRENCY_NAME);
+    return ret;
+}
 
 Scene* HelloWorld::createScene()
 {
     // 'scene' is an autorelease object
     auto scene = Scene::create();
-
+    
     // 'layer' is an autorelease object
     auto layer = HelloWorld::create();
-
+    
     // add layer as a child to scene
     scene->addChild(layer);
-
+    
     // return the scene
     return scene;
 }
@@ -27,9 +43,9 @@ bool HelloWorld::init()
     {
         return false;
     }
-
+    
     CCLOG("Sample Startup");
-
+    
     // add logo
     auto winsize = Director::getInstance()->getWinSize();
     auto logo = Sprite::create("Logo.png");
@@ -37,7 +53,7 @@ bool HelloWorld::init()
     logo->setPosition(Vec2(logoSize.width / 2,
                            winsize.height - logoSize.height / 2));
     addChild(logo);
-
+    
     // add quit button
     auto label = Label::createWithSystemFont("QUIT", "sans", 32);
     auto quit = MenuItemLabel::create(label, [](Ref*){
@@ -47,30 +63,121 @@ bool HelloWorld::init()
     quit->setPosition(Vec2(winsize.width / 2 - labelSize.width / 2 - 16,
                            -winsize.height / 2 + labelSize.height / 2 + 16));
     addChild(Menu::create(quit, NULL));
-
+    
     // add test menu
     createTestMenu();
-
+    
     return true;
 }
 
 void HelloWorld::createTestMenu()
 {
-    auto menu = Menu::create();
-
-    menu->addChild(MenuItemLabel::create(Label::createWithSystemFont("Test Item 1", "sans", 24), [](Ref*){
-        CCLOG("Test Item 1");
-    }));
-
-    menu->addChild(MenuItemLabel::create(Label::createWithSystemFont("Test Item 2", "sans", 24), [](Ref*){
-        CCLOG("Test Item 2");
-    }));
-
-    menu->addChild(MenuItemLabel::create(Label::createWithSystemFont("Test Item 3", "sans", 24), [](Ref*){
-        CCLOG("Test Item 3");
-    }));
-
-    menu->alignItemsVerticallyWithPadding(10);
-    addChild(menu);
+    MenuItemFont::setFontName("sans");
+    auto size = Director::getInstance()->getWinSize();
+    
+    // ui
+    {
+        Menu* menu = Menu::create(
+                                  MenuItemFont::create("requestInterstitial", [](Ref*) {
+            CCLOG("[Fyber] requestInterstitial");
+            PluginFyber::requestInterstitial();
+        }),
+                                  MenuItemFont::create("showOfferWall", [](Ref*) {
+            CCLOG("[Fyber] showOfferWall");
+            PluginFyber::showOfferWall();
+        }),
+                                  MenuItemFont::create("request offers", [](Ref*) {
+            CCLOG("[Fyber] request offers");
+            PluginFyber::requestOffers(DEFAULT_PLACEMENT_ID);
+        }),
+                                  MenuItemFont::create("requestDeltaOfCoins", [](Ref*) {
+            CCLOG("[Fyber] requestDeltaOfCoins");
+            PluginFyber::requestDeltaOfCoins(DEFAULT_PLACEMENT_ID);
+        }),
+                                  NULL);
+        menu->alignItemsVerticallyWithPadding(20);
+        menu->setPosition(size.width/2, size.height/2);
+        addChild(menu);
+        
+        _coins = 0.0f;
+        _coinLabel = Label::createWithSystemFont(make_string_with_coins(_coins), "Arial", 64);
+        _coinLabel->setPosition(size.width/2,100);
+        addChild(_coinLabel);
+    }
+    CCLOG("[Fyber] init.");
+    PluginFyber::init();
+    PluginFyber::setListener(this);
 }
 
+
+// fyber callback
+
+void HelloWorld::onVirtualCurrencyConnectorFailed(int error,
+                                                  const std::string& errorCode,
+                                                  const std::string& errorMsg)
+{
+    CCLOG("[Fyber] onVirtualCurrencyConnectorFailed: error(%d)", error);
+    CCLOG("[Fyber] onVirtualCurrencyConnectorFailed: errorCode(%s)", errorCode.data());
+    CCLOG("[Fyber] onVirtualCurrencyConnectorFailed: errorMsg(%s)", errorMsg.data());
+}
+
+void HelloWorld::onVirtualCurrencyConnectorSuccess(double deltaOfCoins,
+                                                   const std::string& currencyId,
+                                                   const std::string& currencyName,
+                                                   const std::string& transactionId)
+{
+    CCLOG("[Fyber] onVirtualCurrencyConnectorSuccess: deltaOfCoins(%lf)", deltaOfCoins);
+    CCLOG("[Fyber] onVirtualCurrencyConnectorSuccess: currencyId(%s)", currencyId.data());
+    CCLOG("[Fyber] onVirtualCurrencyConnectorSuccess: currencyName(%s)", currencyName.data());
+    CCLOG("[Fyber] onVirtualCurrencyConnectorSuccess: transactionId(%s)", transactionId.data());
+    
+    _coins += deltaOfCoins;
+    _coinLabel->setString(make_string_with_coins(_coins));
+}
+
+void HelloWorld::onCanShowInterstitial(bool canShowInterstitial)
+{
+    CCLOG("[Fyber] onCanShowInterstitial: canShowInterstitial(%s)",
+          BOOL_TO_STRING(canShowInterstitial));
+    
+    if (canShowInterstitial)
+    {
+        PluginFyber::showInterstitial();
+    }
+}
+
+void HelloWorld::onInterstitialDidShow()
+{
+    CCLOG("[Fyber] onInterstitialDidShow");
+}
+
+void HelloWorld::onInterstitialDismiss(const std::string& reason)
+{
+    CCLOG("[Fyber] onInterstitialDismiss: reason(%s)", reason.data());
+}
+
+void HelloWorld::onInterstitialFailed()
+{
+    CCLOG("[Fyber] onInterstitialFailed");
+}
+
+void HelloWorld::onBrandEngageClientReceiveOffers(bool areOffersAvailable)
+{
+    CCLOG("[Fyber] onBrandEngageClientReceiveOffers: available(%s)",
+          BOOL_TO_STRING(areOffersAvailable));
+    if (areOffersAvailable)
+    {
+        PluginFyber::showOffers();
+    }
+}
+
+void HelloWorld::onBrandEngageClientChangeStatus(int status, const std::string& msg)
+{
+    CCLOG("[Fyber] onBrandEngageClientChangeStatus: status(%d)", status);
+    CCLOG("[Fyber] onBrandEngageClientChangeStatus: message(%s)", msg.data());
+}
+
+void HelloWorld::onOfferWallFinish(int status)
+{
+    CCLOG("[Fyber] onOfferWallFinish: status(%d)", status);
+}
--- cpp/Classes/HelloWorldScene.h
+++ cpp/Classes/HelloWorldScene.h
@@ -2,22 +2,41 @@
 #define __HELLOWORLD_SCENE_H__
 
 #include "cocos2d.h"
+#include "PluginFyber/PluginFyber.h"
 
-class HelloWorld : public cocos2d::Layer
+class HelloWorld : public cocos2d::Layer, public sdkbox::FyberListener
 {
 public:
     // there's no 'id' in cpp, so we recommend returning the class instance pointer
     static cocos2d::Scene* createScene();
-
+    
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();
-
+    
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
-
+    
 private:
     void createTestMenu();
-
+    
+    // fyber callback
+    void onVirtualCurrencyConnectorFailed(int error,
+                                          const std::string& errorCode,
+                                          const std::string& errorMsg);
+    void onVirtualCurrencyConnectorSuccess(double deltaOfCoins,
+                                           const std::string& currencyId,
+                                           const std::string& currencyName,
+                                           const std::string& transactionId);
+    void onCanShowInterstitial(bool canShowInterstitial);
+    void onInterstitialDidShow();
+    void onInterstitialDismiss(const std::string& reason);
+    void onInterstitialFailed();
+    void onBrandEngageClientReceiveOffers(bool areOffersAvailable);
+    void onBrandEngageClientChangeStatus(int status, const std::string& msg);
+    void onOfferWallFinish(int status);
+    
+    cocos2d::Label *_coinLabel;
+    double _coins;
 };
 
 #endif // __HELLOWORLD_SCENE_H__
