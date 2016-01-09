a3d53ff2d4a26df4899d103fa953c34ff48f96d5
tinybearc <tinybearc@gmail.com>
2016-01-09 17:00:57 +0800

    update lua

--- lua/src/app/views/MainScene.lua
+++ lua/src/app/views/MainScene.lua
@@ -18,27 +18,69 @@ function MainScene:onCreate()
 end
 
 function MainScene:setupTestMenu()
-    local label1 = cc.Label:createWithSystemFont("Test Item 1", "sans", 28)
-    local item1 = cc.MenuItemLabel:create(label1)
-    item1:onClicked(function()
-        print("Test Item 1")
-    end)
+    local size = cc.Director:getInstance():getWinSize()
 
-    local label2 = cc.Label:createWithSystemFont("Test Item 2", "sans", 28)
-    local item2 = cc.MenuItemLabel:create(label2)
-    item2:onClicked(function()
-        print("Test Item 2")
-    end)
+    cc.MenuItemFont:setFontName('sans')
+    cc.MenuItemFont:setFontSize(32)
 
-    local label3 = cc.Label:createWithSystemFont("Test Item 3", "sans", 28)
-    local item3 = cc.MenuItemLabel:create(label3)
-    item3:onClicked(function()
-        print("Test Item 3")
-    end)
-
-    local menu = cc.Menu:create(item1, item2, item3)
-    menu:alignItemsVerticallyWithPadding(24)
+    local menu = cc.Menu:create(
+        cc.MenuItemFont:create("requestInterstitial"):onClicked(handler(self, self.requestInterstitial)),
+        cc.MenuItemFont:create("showOfferWall"):onClicked(handler(self, self.showOfferWall)),
+        cc.MenuItemFont:create("request offers"):onClicked(handler(self, self.requestOffers)),
+        cc.MenuItemFont:create("requestDeltaOfCoins"):onClicked(handler(self, self.requestDeltaOfCoins))
+    )
+    menu:setPosition(size.width / 2, size.height / 2)
+    menu:alignItemsVerticallyWithPadding(20)
     self:addChild(menu)
+
+    local coinsLabel = cc.Label:createWithSystemFont("0 coins", "Arial", 64)
+    coinsLabel:setPosition(size.width / 2, 100)
+    self:addChild(coinsLabel)
+
+    -- fyber init
+    local coins = 0
+    sdkbox.PluginFyber:init()
+    sdkbox.PluginFyber:setListener(function(args)
+        dump(args)
+        if args.name == "onVirtualCurrencyConnectorFailed" then
+        elseif args.name == "onVirtualCurrencyConnectorSuccess" then
+            coins = coins + args.deltaOfCoins
+            coinsLabel:setString(coins .. " " .. currencyName)
+        elseif args.name == "onCanShowInterstitial" then
+            if args.canShowInterstitial then
+                sdkbox.PluginFyber:showInterstitial()
+            end
+        elseif args.name == "onInterstitialDidShow" then
+        elseif args.name == "onInterstitialDismiss" then
+        elseif args.name == "onInterstitialFailed" then
+        elseif args.name == "onBrandEngageClientReceiveOffers" then
+            if args.areOffersAvailable then
+                sdkbox.PluginFyber:showOffers()
+            end
+        elseif args.name == "onBrandEngageClientChangeStatus" then
+        elseif args.name == "onOfferWallFinish" then
+        end
+    end)
+end
+
+function MainScene:requestInterstitial()
+    print("sdkbox-fyber: requestInterstitial")
+    sdkbox.PluginFyber:requestInterstitial()
+end
+
+function MainScene:showOfferWall()
+    print("sdkbox-fyber: showOfferWall")
+    sdkbox.PluginFyber:showOfferWall()
+end
+
+function MainScene:requestOffers()
+    print("sdkbox-fyber: requestOffers")
+    sdkbox.PluginFyber:requestOffers("rmb")
+end
+
+function MainScene:requestDeltaOfCoins()
+    print("sdkbox-fyber: requestDeltaOfCoins")
+    sdkbox.PluginFyber:requestDeltaOfCoins("rmb")
 end
 
 return MainScene
